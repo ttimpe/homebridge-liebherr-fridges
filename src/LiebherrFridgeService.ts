9a2114fb172d2f5a8611a32f79480e5a61d60946
Tobias Timpe <tobias.timpe@hunter.de>
2020-10-13 16:31:33 +0200

    Preparation for API communication

--- src/LiebherrFridgeService.ts
+++ src/LiebherrFridgeService.ts
@@ -2,17 +2,25 @@ import LiebherrFridge from './LiebherrFridge'
 
 export default class LiebherrFridgeService {
 
+	private accessToken: string
+	private refreshToken: string
+
 	async loginUser(email: string, password: string) {
+
+		const url = ''
 
 	}
 
 	async getDevices() {
 		let devices: LiebherrFridge[] = []
+
+		const url = ''
+
 		return devices
 	}
 	
 	async updateDeviceValue() {
-
+		const url = ''
 	}
 
 }
